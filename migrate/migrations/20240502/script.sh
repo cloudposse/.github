1f0965f56782b9c022d669376c53d3194ab1d604
Igor Rodionov <goruha@gmail.com>
2024-05-03 17:50:56 +0300

    Update script.sh

--- migrate/migrations/20240502/script.sh
+++ migrate/migrations/20240502/script.sh
@@ -4,6 +4,7 @@ install_github_settings
 install .github/workflows
 remove .github/workflows/auto-readme.yml
 remove .github/workflows/auto-release.yml
+remove .github/auto-release.yml
 
 # Merge the PR
 auto_merge
