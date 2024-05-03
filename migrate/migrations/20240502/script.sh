80046bd3d1cbc648094090eda10da26f1070258d
Igor Rodionov <goruha@gmail.com>
2024-05-03 17:48:57 +0300

    Update script.sh

--- migrate/migrations/20240502/script.sh
+++ migrate/migrations/20240502/script.sh
@@ -3,6 +3,7 @@ title "Use GitHub Action Workflows from \`cloudposse/.github\` Repo"
 install_github_settings
 install .github/workflows
 remove .github/workflows/auto-readme.yml
+remove .github/workflows/auto-release.yml
 
 # Merge the PR
 auto_merge
