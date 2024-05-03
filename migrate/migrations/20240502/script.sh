9877619cc2c225120a48676bca216aaf7b180fe5
Igor Rodionov <goruha@gmail.com>
2024-05-03 18:30:46 +0300

    Update script.sh

--- migrate/migrations/20240502/script.sh
+++ migrate/migrations/20240502/script.sh
@@ -2,6 +2,9 @@ title "Use GitHub Action Workflows from \`cloudposse/.github\` Repo"
 
 install_github_settings
 install .github/workflows
+remove .github/workflows/feature-branch.yaml
+remove .github/workflows/main-branch.yaml
+remove .github/workflows/release.yaml
 remove .github/workflows/auto-readme.yml
 remove .github/workflows/auto-release.yml
 remove .github/workflows/release-published.yml
