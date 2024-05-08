354e294f0787272182eaec7085e6b66ecea5d0e1
Igor Rodionov <goruha@gmail.com>
2024-05-08 23:18:06 +0300

    Fix delete branch protection

--- migrate/lib/github.sh
+++ migrate/lib/github.sh
@@ -1,5 +1,5 @@
 
 function delete_branch_protection() {
     local default_branch=$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)
-    gh api -X DELETE /repos/${XARGS_REPO_OWNER}/${XARGS_REPO_NAME}/branches/${default_branch}/protection
+    gh api -X DELETE /repos/${XARGS_REPO_OWNER}/${XARGS_REPO_NAME}/branches/${default_branch}/protection || true
 }
