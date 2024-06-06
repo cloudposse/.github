6bd98965be9c2a7475717cb4de088fac95f9137b
Igor Rodionov <goruha@gmail.com>
2024-06-06 18:56:07 +0200

    Drop unnecessary migration

--- migrate/migrations/20240523/repos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gh repo list cloudposse --limit 500 --json name,owner \
-    --jq '.[] | select(.name | test("^terraform")) | select(.name | test("^terraform-provider") | not) | select(.name | test("^terraform-aws-components") | not) | .owner.login + "/" + .name' > repos.txt
-
-split -d -l 16 repos.txt repos-
--- migrate/migrations/20240523/script.sh
+++
@@ -1,4 +0,0 @@
-title "Update terratest to '>= 0.46.0'"
-
-update_terratest
-
