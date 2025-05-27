981bb55cea0312f218f216510144d07952d32351
Igor Rodionov <goruha@gmail.com>
2025-05-27 22:59:17 +0300

    Use atmos to generate readme (#175)
    * Use atmos to generate readme
    
    * Get rid of build harness
    
    * Update shared-terraform-module.yml
    
    * Update shared-terraform-module.yml
    
    * Create atmos.terraform-module.yaml
    
    * Migrate readme generation to atmos
    
    * Update shared-terraform-module.yml
    
    * Update shared-readme.yml
    
    * Update shared-terraform-module.yml
    
    * Update shared-readme.yml
    
    * Update shared-terraform-module.yml
    
    * Update shared-terraform-module.yml
    
    * Rename .github/atmos.terraform-module.yaml to .github/atmos/terraform-module.yaml
    
    * Update terraform-module.yaml
    
    * Update atmos.yaml
    
    * Apply suggestions from code review
    
    * Update terraform-module.yaml
    
    ---------
    
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

---
+++ migrate/migrations/20250523/repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gh repo list cloudposse --limit 500 --json name,owner \
+    --jq '.[] | select(.name | test("^terraform")) | select(.name | test("^terraform-provider") | not) | select(.name | test("^terraform-aws-components") | not) | .owner.login + "/" + .name' > repos.txt
+
+split -d -l 16 repos.txt repos-
