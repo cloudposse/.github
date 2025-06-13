ae7f0ee99c32d8c68bcdd30b354a27a4777891ab
Igor Rodionov <goruha@gmail.com>
2025-06-13 22:28:13 +0200

    Run migrations only if  dir exists

--- migrate/migrations/20250606/script.sh
+++ migrate/migrations/20250606/script.sh
@@ -1,10 +1,15 @@
 title "Fix go version in tests"
 
-cd test/src
-GOTOOLCHAIN=auto go mod edit -go=1.24 -toolchain=go1.24.0
-GOTOOLCHAIN=auto go mod tidy
-GOTOOLCHAIN=auto go test -v -run Skip		
-cd -
+if [ -d "test/src" ]; then
+  cd test/src
+  GOTOOLCHAIN=auto go mod edit -go=1.24 -toolchain=go1.24.0
+  GOTOOLCHAIN=auto go mod tidy
+  GOTOOLCHAIN=auto go test -v -run Skip
+  cd -
 
-# Merge the PR
-auto_merge
+	# Merge the PR
+	auto_merge
+else
+  echo "test/src directory not found"
+fi
+
