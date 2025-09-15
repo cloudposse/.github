a5c51e643f8f218ba30b0e7bda2cefb11b90507d
Igor Rodionov <goruha@gmail.com>
2025-09-15 23:09:42 +0200

    Remove github-action-atmos-dependencies-test GHA

--- migrate/migrations/20250915/repos.sh
+++ migrate/migrations/20250915/repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gh repo list cloudposse --limit 500 --json name,owner \
-	--jq '.[] | select(.name | test("^github-action-")) | .owner.login + "/" + .name' > repos.txt
+	--jq '.[] | select(.name | test("^github-action-")) | select(.name | test("github-action-atmos-dependencies-test") | not) | .owner.login + "/" + .name' > repos.txt
 
 split -d -l 16 repos.txt repos-
